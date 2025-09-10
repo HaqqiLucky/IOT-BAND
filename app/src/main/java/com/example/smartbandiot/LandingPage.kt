a200cd02f408da900c56ae2d6e5402b8b379005f
HaqqiLucky <haqqiarifin@outlook.com>
2025-09-10 15:16:53 +0700

    init me culpa

---
+++ app/src/main/java/com/example/smartbandiot/LandingPage.kt
@@ -0,0 +1,25 @@
+package com.example.smartbandiot
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.smartbandiot.databinding.LandingPageBinding
+
+class LandingPage : AppCompatActivity() {
+    private lateinit var binding: LandingPageBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = LandingPageBinding.inflate(layoutInflater)
+
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
