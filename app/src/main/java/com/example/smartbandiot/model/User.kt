0561219e9226d8163488c24862b7cea0ae311bfd
HaqqiLucky <haqqiarifin@outlook.com>
2025-10-22 18:21:43 +0700

    kayaknya rulebase done deh

--- app/src/main/java/com/example/smartbandiot/model/User.kt
+++ app/src/main/java/com/example/smartbandiot/model/User.kt
@@ -3,18 +3,17 @@ package com.example.smartbandiot.model
 import com.google.firebase.auth.FirebaseAuth
 import java.nio.file.Path
 
-// Data class untuk menyimpan informasi user
 val auth = FirebaseAuth.getInstance()
 val firebaseUser = auth.currentUser
 data class User (
     val uid: String = firebaseUser?.uid ?: "",
     val name: String = firebaseUser?.displayName ?: "",
-    val weight: Double, //
-    val height: Double, //
+    val weight: Double = 0.0, //
+    val height: Double = 0.0, //
     var birthYYYYmm: String = "", //
-    val profileImagePath: String,
-    val email: String,
-    val gender: String, //
-    val goal: String //
+    val profileImagePath: String = "",
+    val email: String = "",
+    val gender: String = "", //
+    val goal: String = "" //
 )
 // hy leni dan jawa kalo klean baca ini aku mau kasi tau kalo beberapa variabel ini krusial karena di pake buat rule based,yg aku komen beraarri sangat berharga
